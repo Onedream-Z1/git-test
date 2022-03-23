4d7eadd2dbe7680a0c75552b3efc4f3e76b67b2b
xz <xz@qq.com>
2022-03-23 19:26:30 +0800

    second commit

--- src/main/java/cn/xz/gitest/GitTest.java
+++ src/main/java/cn/xz/gitest/GitTest.java
@@ -3,5 +3,6 @@ package cn.xz.gitest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git....");
+        System.out.println("hello git2...");
     }
 }
